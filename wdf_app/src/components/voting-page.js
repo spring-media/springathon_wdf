fff3c923a9d65b9d91be9fe0ae2654af84539002
Monika Ibovnik <monika.ibovnik@axelspringer.com>
2019-08-19 20:02:43 +0200

    add carousel

--- wdf_app/src/components/voting-page.js
+++ wdf_app/src/components/voting-page.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 import ReactPlayer from 'react-player'
+import AwesomeSlider from 'react-awesome-slider'
+import AwsSliderStyles from 'react-awesome-slider/src/styles';
 
 export function VotingPage() {
   return (
     <div>
-      <h1>voting page</h1>
-        <ReactPlayer url='sample-video.mp4' controls/>
+      voting page
+      <AwesomeSlider cssModule={AwsSliderStyles}>
+        <div>
+          video1
+          <ReactPlayer url="sample-video.mp4" controls />
+        </div>
+        <div>
+          video2
+          <ReactPlayer url="sample-video.mp4" controls />
+        </div>
+      </AwesomeSlider>
       <Button variant="outlined" color="primary" onClick={null}>
         Like
       </Button>
