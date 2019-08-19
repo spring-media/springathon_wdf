910e8947c3fd35e02d53b117cdc900c74d47c979
Monika Ibovnik <monika.ibovnik@axelspringer.com>
2019-08-19 20:37:05 +0200

    add fab and icons

--- wdf_app/src/components/voting-page.js
+++ wdf_app/src/components/voting-page.js
@@ -2,25 +2,33 @@ import React from 'react'
 import { Button } from '@material-ui/core'
 import ReactPlayer from 'react-player'
 import AwesomeSlider from 'react-awesome-slider'
-import AwsSliderStyles from 'react-awesome-slider/src/styles';
+import AwsSliderStyles from 'react-awesome-slider/src/styles'
+import { makeStyles } from '@material-ui/styles'
+import Fab from '@material-ui/core/Fab'
+import ThumbUp from '@material-ui/icons/ThumbUp'
 
 export function VotingPage() {
+  const classes = useStyles()
   return (
-    <div>
-      voting page
-      <AwesomeSlider cssModule={AwsSliderStyles}>
+    <div className={classes.container}>
+      <AwesomeSlider className={classes.container} bullets={false}>
         <div>
-          video1
-          <ReactPlayer url="sample-video.mp4" controls />
+          <ReactPlayer url="sample-video.mp4" controls width={400} height={250} />
         </div>
         <div>
           video2
           <ReactPlayer url="sample-video.mp4" controls />
         </div>
       </AwesomeSlider>
-      <Button variant="outlined" color="primary" onClick={null}>
-        Like
-      </Button>
+      <Fab>
+        <ThumbUp />
+      </Fab>
     </div>
   )
 }
+
+const useStyles = makeStyles({
+  container: {
+    width: '400px',
+  },
+})
